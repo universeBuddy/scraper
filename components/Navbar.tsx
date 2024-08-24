c7643291d74fd5db31d7c1fb425b3fb17a461bea
universeBuddy <rai358788@gmail.com>
2024-08-24 22:06:41 +0530

    navbar created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,43 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
-
-const navIcon =[
-  {src: '/assests/icons/search.svg', alt:'search'}
-  {src: '/assests/icons/black-herat.svg', alt:'heart'}
-  {src: '/assests/icons/user.svg', alt:'user'}
-  
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
 ]
+
 const Navbar = () => {
   return (
-   <header className='w-full'>
-    <nav className='nav'>
-      <Link href= "/" className=" flex items-center gap-1 ">
-      <Image src="/assets/icons/logo.svg"
-      width={27}
-      height={27}
-      alt='logo' />
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image 
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
 
-      <p className='nav-logo'>
-        Scrapy
-      </p>
-      </Link>
-      
-      
-      </nav> 
+          <p className="nav-logo">
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
 
-   </header>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image 
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
   )
 }
 
