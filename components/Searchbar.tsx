dac36d5bf25e563c4c4038f7468aad2d649d06a7
universeBuddy <rai358788@gmail.com>
2024-08-25 09:15:41 +0530

    adding searchbar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import React from "react";
+import React, { useState, FormEvent } from "react";
 
-const handleSubmit = () => {};
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon") ||
+      hostname.endsWith("amazon")
+    )
+      return true;
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
+
 const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState("");
+
+  const [isLoading, setIsLoading] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    alert(isValidLink ? "Valid Link" : " Invaid link");
+  };
+
+  try {
+    setIsLoading(true);
+  } catch (error) {}
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
         className=" searchbar-input"
         placeholder="Enter the Product Link"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
       />
       <button type="submit" className="searchbar-btn">
         search
