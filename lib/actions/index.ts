b082f86e1c2c392bf8fa8f01dce09ef868b9ca8e
universeBuddy <rai358788@gmail.com>
2024-08-25 20:48:00 +0530

    finding bug

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,17 +2,34 @@
 
 import { scrapeAmazonProduct } from "@/lib/scrapy";
 import { connectToDB } from "../mongoose";
+import Product from "../models/product-model";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
+
+
   try {
-   connectToDB();
+    connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
     if (!scrapedProduct) return;
 
+    const existingProduct = await Product.findOne({
+     url:scrapedProduct.url
+    });
 
-    const existingProduct  = await Product.
+    if (existingProduct) {
+      const updatePriceHistory = [
+        ...existingProduct.priceHistory,
+        {
+          price: scrapedProduct.currentPrice
+        },
+      ];
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+      };
+    }
   } catch (error: any) {
     throw new Error(`Faild to create/update product:${error.message}`);
   }
