0aefa3d18ce52c2ff99e76338a95e77c23701207
universeBuddy <rai358788@gmail.com>
2024-08-25 19:24:13 +0530

    model schema added

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,13 +1,19 @@
 "use server";
 
 import { scrapeAmazonProduct } from "@/lib/scrapy";
+import { connectToDB } from "../mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
   try {
+   connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+
+
+    const existingProduct  = await Product.
   } catch (error: any) {
     throw new Error(`Faild to create/update product:${error.message}`);
   }
 }
- 
---
+++ lib/mongoose.ts
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from "mongoose";
+
+
+let isConnected =false;
+
+export const connectToDB  = async ()=>{
+  mongoose.set('strictQuery',true);
+
+  if(!process.env.MONGO_URI) return console.log('MONGO_DB is not define');
+
+  if(isConnected) return console.log(` > using existing database connection `)
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+
+        isConnected  =true;
+
+console.log('MongoDB Connected');
+
+
+
+
+    } catch (error) {
+        
+    }
+
+
+
+
+
+}
