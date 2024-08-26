2d2aa47aa24d772fc16021be97ca3dae194eda6e
universeBuddy <rai358788@gmail.com>
2024-08-26 15:30:26 +0530

    creating product details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { getProductById } from "@/lib/actions";
+import { formatNumber } from "@/lib/utils";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const ProductDetails = () => {
+type Props = {
+  params: { id: string };
+};
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById(id);
+
+  if (!product) redirect("/");
+
   return (
-    <div>ProductDetails</div>
-  )
-}   
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={380}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
 
-export default ProductDetails
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">
+                {product.title}
+              </p>
+
+              <Link
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+            <div className="flex item-center gap-3">
+              <div className="product-heart">
+                <Image
+                  src="/assets/icons/red-heart.svg"
+                  alt="herat"
+                  width={20}
+                  height={20}
+                />
+
+                <p className="text-base font-semibold text-[#D46F77]">
+                  {product.reviewsCount}
+                </p>
+              </div>
+
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src="/assets/icons/bookmark.svg"
+                  alt="bookmark"
+                  width={20}
+                  height={20}
+                />
+              </div>
+
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src="/assets/icons/share.svg"
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="product-info">
+ 
+          <div className="flex flex-col gap-2">
+              <p className="text-[34px] text-secondary font-bold">
+                {product.currency} {formatNumber(product.currentPrice)}
+              </p>
+              <p className="text-[21px] text-black opacity-50 line-through">
+                {product.currency} {formatNumber(product.originalPrice)}
+              </p>
+            </div>
+
+
+<div className="">
+  
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
